63b41c85fa80d8359aeb91aae005ce5f4d4f4d0e
Gergoa123 <gergo3124@gmail.com>
2022-05-02 17:47:09 +0300

    Add project files.

---
+++ Exercitiul 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercitiul_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti trei numere: ");
+            int numar1 = Convert.ToInt32(Console.ReadLine());
+            int numar2 = Convert.ToInt32(Console.ReadLine());
+            int numar3 = Convert.ToInt32(Console.ReadLine());
+
+            int media = (numar1 + numar2 + numar3) / 3;
+            Console.WriteLine("Media aritmetica este = " + media);
+        }
+    }
+}
